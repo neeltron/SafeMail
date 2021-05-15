71b490464422567635875d46af19815ebdb6b711
unknown <500075823@stu.upes.ac.in>
2021-05-15 14:27:37 +0530

    Activate Camera when Motion is detected

--- SafeMail.py
+++ SafeMail.py
@@ -6,10 +6,13 @@ Created on Sat May 15 11:57:16 2021
 """
 
 import serial
+import cv2
+
 serial_distance = serial.Serial('COM5')
 serial_distance.flushInput()
 count = 0
 count_valid = 0
+live = cv2.VideoCapture(0)
 
 while True:
     try:
@@ -17,12 +20,19 @@ while True:
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         print(decoded_bytes)
         count_valid += 1
-        if decoded_bytes < 8 and count_valid > 100:
+        if decoded_bytes < 8 and count_valid > 2:
+            ret, frame = live.read()
+            color = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('frame', color)
             print("Action triggered!")
             serial_distance.close()
+            cv2.imwrite('image.jpg', frame)
             break
     except:
         print("Error")
         count += 1
         if count > 100:
             break
+
+live.release()
+cv2.destroyAllWindows()
