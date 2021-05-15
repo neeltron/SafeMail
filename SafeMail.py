3b87fcd85e8b0908562bc3c380288d205832be99
unknown <500075823@stu.upes.ac.in>
2021-05-15 13:12:34 +0530

    Added Action Trigger slot

--- SafeMail.py
+++ SafeMail.py
@@ -9,12 +9,18 @@ import serial
 serial_distance = serial.Serial('COM5')
 serial_distance.flushInput()
 count = 0
+count_valid = 0
 
 while True:
     try:
         ser_bytes = serial_distance.readline()
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         print(decoded_bytes)
+        count_valid += 1
+        if decoded_bytes < 8 and count_valid > 100:
+            print("Action triggered!")
+            serial_distance.close()
+            break
     except:
         print("Error")
         count += 1
